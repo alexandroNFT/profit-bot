f830129c9cda7612385cedf1c39d255c91967837
alexandroNFT <alexandro@smartseasociety.com>
2023-03-16 11:20:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const { Client } = require('pg');
+const Eris = require("eris");
+const config = require("./config.json");
+
+const bot = new Eris(config.botToken);
+
+const dbClient = new Client({
+  connectionString: config.connectionString,
+});
+
+let lastSignalId = 0; // Keep track of the last sent signal ID
+
+async function getSignalsWithPriceRise() {
+  const query = `
+    SELECT s.id, s.highest_floor_price_after, s.floor_price, s.timestamp, c.name, c.image
+    FROM ml_signals s
+    JOIN collections c ON s.collection_id = c.id
+    WHERE (s.highest_floor_price_after - s.floor_price) / s.floor_price > 0.4
+    AND s.timestamp > NOW() - INTERVAL '1 week'
+    AND s.id > $1;
+  `;
+
+  const result = await dbClient.query(query, [lastSignalId]);
+  return result.rows;
+}
+
+function formatTimeDifference(timestamp) {
+  const now = new Date();
+  const difference = now - new Date(timestamp);
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((difference / (1000 * 60)) % 60);
+
+  let formattedDifference = '';
+  if (days > 0) {
+    formattedDifference += `${days}d `;
+  }
+  if (hours > 0) {
+    formattedDifference += `${hours}h `;
+  }
+  if (minutes > 0) {
+    formattedDifference += `${minutes}m`;
+  }
+
+  return formattedDifference;
+}
+
+async function processSignals(channel) {
+  const signals = await getSignalsWithPriceRise();
+
+  if (signals.length > 0) {
+    for (const signal of signals) {
+      const { id, highest_floor_price_after, floor_price, timestamp, name, image } = signal;
+      const period = formatTimeDifference(timestamp);
+
+      const embed = {
+        description: ` ${name} Take-Profit target ✅ \nProfit: 40+% 📈\nPeriod: ${period} ⏰ `,
+        thumbnail: { url: image },
+      };
+
+      channel.createMessage({ embed });
+      lastSignalId = id;
+    }
+  }
+}
+
+bot.on('ready', async () => {
+  console.log('Discord connected succesfully');
+
+  await dbClient.connect(); // Move this line here
+
+  const targetChannelId = '1085547490754953337';
+  const targetChannel = bot.getChannel(targetChannelId);
+
+  setInterval(() => {
+    processSignals(targetChannel);
+  }, 5 * 60 * 1000); // Check every 5 minutes
+});
+
+bot.on("error", (err) => {
+  console.error(err);
+});
+
+bot.connect();
